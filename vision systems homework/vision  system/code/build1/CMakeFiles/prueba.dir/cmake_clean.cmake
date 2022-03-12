4d049a573b7cb85d75b1ca0f389ad21097a8cf81
Felix Velazco <79220309+felixVelazco@users.noreply.github.com>
2022-03-12 16:46:18 -0700

    Add: Mafile

---
+++ vision systems homework/vision  system/code/build1/CMakeFiles/prueba.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/prueba.dir/mejoramiento.cpp.o"
+  "prueba.pdb"
+  "prueba"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/prueba.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
